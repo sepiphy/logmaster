41fa7e345da3b2480004209635fda6c41a4fb541
Quynh Xuan Nguyen <seriquynh@gmail.com>
2020-08-20 16:20:50 +0700

    Add more tests

--- tests/LogMaster.test.js
+++ tests/LogMaster.test.js
@@ -3,24 +3,40 @@ const assert = require('assert');
 
 describe('LogMaster', function () {
   describe('#log()', function () {
-    it('suold call the log callback and pass formated message.', function () {
-      const log = LogMaster({
-        name: 'Test',
-      });
+    it('should call the log callback and pass formated message.', function () {
+      const log = LogMaster({ name: 'Test' });
       log.logUsing(function (message, context) {
-        assert.equal(true, message.indexOf('Running test') !== -1);
+        assert.equal(message.indexOf('Running test') !== -1, true);
       });
       log.info('Running test');
     });
-    it('suold call the log callback and pass formated message and context.', function () {
-      const log = LogMaster({
-        name: 'Test',
-      });
+    it('should call the log callback and pass formated message and context.', function () {
+      const log = LogMaster({ name: 'Test' });
       log.logUsing(function (message, context) {
-        assert.equal(true, message.indexOf('Running test') !== -1);
-        assert.equal('LogMaster', context.name);
+        assert.equal(message.indexOf('Running test') !== -1, true);
+        assert.equal(context.name, 'LogMaster');
       });
       log.info('Running test', { name: 'LogMaster' });
+    });
+  });
+
+  describe('#disableUsing()', function () {
+    it('should set disable condition.', function () {
+      const log = LogMaster({ name: 'Test' });
+      let count = 0;
+      log.logUsing(function (message, context) {
+        count = count + 1;
+      });
+      log.disableUsing(function () {
+        return true;
+      });
+      log.info('Running test');
+      assert.equal(count, 0);
+      log.disableUsing(function () {
+        return false;
+      });
+      log.info('Running test');
+      assert.equal(count, 1);
     });
   });
 });
